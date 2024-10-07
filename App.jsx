79a6dbfcbfd7534152556c8189e594f2e3d47fe7
Limhari0301 <imhari030@gmail.com>
2024-10-08 02:41:35 +0900

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,26 +1,35 @@
-import React from "react";
-import { MOVIES } from "./mocks/movies"; // 영화 데이터 import
-import './App.css'; // CSS 파일 import (위의 CSS 코드가 여기에 포함되어야 함)
+import {useEffect, useState} from "react";
+import axios from "axios"; // yarn add axios 명령어 실행
+import * as S from './movies.style.js'
 
-const MovieList = () => {
-  const twoRowsOfMovies = MOVIES.results.slice(0, 20); // 영화 데이터 가져오기
+const MoviesPage = () => {
+    const [movies, setMovies] = useState([])
 
-  return (
-    <div>
-      <div style={{ display: 'flex', flexWrap: 'wrap' }}>
-        {twoRowsOfMovies.map((movie) => (
-          <div key={movie.id} className="movie-container">
-            <img
-              className="movie-image"
-              src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-              alt={movie.title}
-            />
-            <div className="overlay"></div> {/* 오버레이 추가 */}
-          </div>
-        ))}
-      </div>
-    </div>
+    useEffect(() => {
+        const getMovies = async () => {
+            const movies = await axios.get(`https://api.themoviedb.org/3/movie/popular?language=en-US&page=1`, {
+                headers: {
+                    Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjNTNkYWIyMDkxMzI2Y2Y3NTkwNTAwYjQyODNkNjZkNyIsIm5iZiI6MTcyNjE0MTU3Ny42MDM2ODcsInN1YiI6IjY0MzVmY2Y2NjUxZmNmMDBkM2RhYzNmNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.cFPsPRHPidq2OnJ3U-3wHJYhnGajDFqUsM8XJ_a_0bw`
+                }
+            })
+            setMovies(movies.data.results);
+        }
+        getMovies()
+    }, []);
+
+    return (
+      <S.CardList>
+          {movies.map((movie) => (
+              <S.Card key={movie.id}>
+                  <S.Image 
+                      src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`} 
+                      alt={movie.title}
+                  />
+                  <S.Title>{movie.title}</S.Title>
+              </S.Card>
+          ))}
+      </S.CardList>
   );
 };
 
-export default MovieList;
+export default MoviesPage;
